7edecffc4179ccd6331bffd8631cebfe0a85aaee
rohan-04 <mathurrohan04@gmail.com>
2021-06-18 21:10:02 +0530

    small fix for api

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import './App.css';
+
 import Nav from '../Nav/Nav';
 import Landing from '../Landing/Landing';
 import ChallengeSection from '../ChallengeSection/ChallengeSection';
 import Footer from '../Footer/Footer';
+import { SAMPLE_PARAGRAPHS } from '../../data/sampleParagraphs';
+import './App.css';
 
 const TotalTime = 60;
-const ServiceUrl =
-	'https://baconipsum.com/api/?type=all-meat&paras=3&start-with-lorem=1&format=text';
+// const ServiceUrl = 'https://baconipsum.com/api/?type=all-meat&paras=3&start-with-lorem=1&format=text';
 
 const DefaultState = {
 	selectedParagraph: '',
@@ -22,24 +23,38 @@ const DefaultState = {
 class App extends React.Component {
 	state = DefaultState;
 
-	fetchNewParagraph = () => {
-		fetch(ServiceUrl)
-			.then((response) => response.text())
-			.then((data) => {
-				const selectedParagraphArray = data.split('');
-				const testInfo = selectedParagraphArray.map((selectedLetter) => {
-					return {
-						testLetter: selectedLetter,
-						status: 'notAttempted',
-					};
-				});
+	fetchNewParagraphFallback = () => {
+		const data =
+			SAMPLE_PARAGRAPHS[Math.floor(Math.random() * SAMPLE_PARAGRAPHS.length)];
+		const selectedParagraphArray = data.split('');
+		const testInfo = selectedParagraphArray.map((selectedLetter) => {
+			return {
+				testLetter: selectedLetter,
+				status: 'notAttempted',
+			};
+		});
 
-				this.setState({ ...DefaultState, testInfo, selectedParagraph: data });
-			});
+		this.setState({ ...DefaultState, testInfo, selectedParagraph: data });
 	};
 
+	// fetchNewParagraph = () => {
+	// 	fetch(ServiceUrl)
+	// 		.then((response) => response.text())
+	// 		.then((data) => {
+	// 			const selectedParagraphArray = data.split('');
+	// 			const testInfo = selectedParagraphArray.map((selectedLetter) => {
+	// 				return {
+	// 					testLetter: selectedLetter,
+	// 					status: 'notAttempted',
+	// 				};
+	// 			});
+
+	// 			this.setState({ ...DefaultState, testInfo, selectedParagraph: data });
+	// 		});
+	// };
+
 	componentDidMount() {
-		this.fetchNewParagraph();
+		this.fetchNewParagraphFallback();
 	}
 
 	startTimer = () => {
@@ -63,7 +78,7 @@ class App extends React.Component {
 	};
 
 	// Try Again
-	startAgain = () => this.fetchNewParagraph();
+	startAgain = () => this.fetchNewParagraphFallback();
 
 	handleUserInput = (inputValue) => {
 		if (!this.state.timeStarted) this.startTimer();
